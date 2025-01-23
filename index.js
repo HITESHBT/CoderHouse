a758773483ef6299388ede8067d7393d7c568386
HITESH SHARMA <hitesh.hackathons@gmail.com>
2025-01-24 03:06:58 +0530

    trying to deploy on vercel

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const express=require('express')
-const app=express()
+const express = require('express');
 const mongoose = require('mongoose');
-const Employee= require('./models/Employee.model')
-require('dotenv').config()
-app.use(express.json())
-const employeeRoute=require('./routes/Employee.route')
-const adminRoute=require('./routes/Admin.route')
-app.use(express.urlencoded({extended:false}))
-app.use('/Employee',employeeRoute)
-app.use('/Admin',adminRoute)
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
-    console.log("database is connected")
-   
-}).catch((err)=>{
-   console.error(err); 
-})
-app.listen(3000,()=>{
-    console.log('the server has started');
+const Employee = require('./models/Employee.model');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const employeeRoute = require('./routes/Employee.route');
+const adminRoute = require('./routes/Admin.route');
+
+app.use('/Employee', employeeRoute);
+app.use('/Admin', adminRoute);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Database is connected');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.get('/', (req, res) => {
+  res.send('Hello from CoderHouse');
 });
 
-app.get('/',(req,res)=>{
-    res.send("hello from CoderHouse")
-})
+// Export the app wrapped with serverless-http
+const serverless = require('serverless-http');
+module.exports = serverless(app);
